56ae9c5ef84f1d4bc55e05821323c0a03c1e286f
Irho <irhad.saric@hotmail.com>
2019-11-04 16:35:43 +0100

    Cloud collector added

---
+++ Assets/Scripts/Cloud Collectors Scripts/CloudCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCollector : MonoBehaviour
+{
+    void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == "Cloud" || target.tag == "Deadly")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] clouds;
+
+    private float distanceBetweenClouds = 3f;
+
+    private float minX, maxX;
+
+    private float lastCloudPositionY;
+
+    private float controlX;
+
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject[] collectables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
